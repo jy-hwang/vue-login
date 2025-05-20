23285992301eb09c36f707640d2cada2934ff458
jy-hwang <stratojy@gmail.com>
2025-05-20 16:14:11 +0900

    feat. 로그인 유지를 위한 쿠키에 값 저장
    1. 이것은 굉장히 보안상 취약하며 좋지 않은 방법임.
    2. 쿠키에 로그인 정보를 저장함.
    3. 로그인 시도시 쿠키에 정보를 확인하여, 있으면 쿠키 정보를 반환.

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const members = [
   {
@@ -19,9 +22,15 @@ const members = [
   },
 ];
 
-app.use(bodyParser.json());
-
 app.get("/api/account", (req, res) => {
+  console.log(req.cookies);
+  if (req.cookies && req.cookies.account) {
+    const member = JSON.parse(req.cookies.account);
+
+    if (member.id) {
+      return res.send(member);
+    }
+  }
   res.send(401);
 });
 
@@ -34,6 +43,12 @@ app.post("/api/account", (req, res) => {
   );
 
   if (member) {
+    const options = {
+      domain: "localhost",
+      path: "/",
+      httpOnly: true,
+    };
+    res.cookie("account", JSON.stringify(member), options);
     res.send(member);
   } else {
     res.status(401).send("계정정보를 찾을 수 없습니다.");
